3cefad11c547e90de1203e5fdb354c98d44c928e
Thanatcha Panpairoj <panpairojthanatcha@berkeley.edu>
2016-09-08 17:40:15 -0700

    Color adjustment.

--- src/Vertex.java
+++ src/Vertex.java
@@ -21,7 +21,7 @@ public class Vertex
         this.depthScale = s * WIDTH / (10 + z);
         this.twoDX = (this.depthScale * x);
         this.twoDY = (this.depthScale * y);
-	this.normal = new Point(0, 0, 0);
+	    this.normal = new Point(0, 0, 0);
     }
 
     public Vertex(double x, double y, double z) {
@@ -29,14 +29,14 @@ public class Vertex
     }
 
     public void addNormal(Point normal) {
-	this.normal = new Point((this.normal.getX() * normalsAdded + normal.getX()) / (normalsAdded + 1), 
+	    this.normal = new Point((this.normal.getX() * normalsAdded + normal.getX()) / (normalsAdded + 1), 
 				(this.normal.getY() * normalsAdded + normal.getY()) / (normalsAdded + 1),
 				(this.normal.getZ() * normalsAdded + normal.getZ()) / (normalsAdded + 1));
-	normalsAdded += 1;
-	//double mag = Math.sqrt(Math.pow(this.normal.getX(), 2) + Math.pow(this.normal.getY(), 2) + Math.pow(this.normal.getZ(), 2));
-	//System.out.println(this.normal.getX()+ " " + this.normal.getY() + " " + this.normal.getZ() + " " + mag);
-	//this.normal = new Point(this.normal.getX() / mag, this.normal.getY() / mag, this.normal.getZ() / mag);
-	this.lightingScaleConstant = 0.7 / (0.3266667 * Math.sqrt(Math.pow(this.normal.getX(), 2) + Math.pow(this.normal.getY(), 2) + Math.pow(this.normal.getZ(), 2))); 
+	    normalsAdded += 1;
+	    //double mag = Math.sqrt(Math.pow(this.normal.getX(), 2) + Math.pow(this.normal.getY(), 2) + Math.pow(this.normal.getZ(), 2));
+	    //System.out.println(this.normal.getX()+ " " + this.normal.getY() + " " + this.normal.getZ() + " " + mag);
+	    //this.normal = new Point(this.normal.getX() / mag, this.normal.getY() / mag, this.normal.getZ() / mag);
+	    this.lightingScaleConstant = 0.7 / (0.3266667 * Math.sqrt(Math.pow(this.normal.getX(), 2) + Math.pow(this.normal.getY(), 2) + Math.pow(this.normal.getZ(), 2))); 
     }
 
     public void transform(double[] transformationMatrix) {
@@ -54,36 +54,36 @@ public class Vertex
     }
 
     public void transformNormal(double[] transformationMatrix) {
-	normal.transform(transformationMatrix);
+	    normal.transform(transformationMatrix);
     }
 
     public void calculateNewlightingScale(double gravityX, double gravityY, double gravityZ) {
         lightingScale = Math.max((gravityX * normal.getX() + gravityY * normal.getY() + gravityZ * normal.getZ()) * lightingScaleConstant, -0.4);
-	r = 124 + (int)(131 * lightingScale);
-	g = 125 + (int)(130 * lightingScale);
-	b = 135 + (int)(120 * lightingScale);
+	    r = 124 + (int)(111 * lightingScale);
+	    g = 135 + (int)(110 * lightingScale);
+	    b = 150 + (int)(100 * lightingScale);
     }
 
     public void setRGB(int r, int g, int b) {
-	this.r = r;
-	this.g = g;
-	this.b = b;
+	    this.r = r;
+	    this.g = g;
+	    this.b = b;
     }
 
     public Point getNormal() {
-	return normal;
+	    return normal;
     }
 
     public int getR() {
-	return r;
+	    return r;
     }
 
     public int getG() {
-	return g;
+	    return g;
     }
 
     public int getB() {
-	return b;
+	    return b;
     }
 
     public double get2Dx() {
